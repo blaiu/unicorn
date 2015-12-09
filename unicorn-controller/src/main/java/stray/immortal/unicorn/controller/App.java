efcced0813b0e3c60d00f988c67a9c804331cb5d
blaiu <blaiu@163.com>
2015-12-09 17:22:15 +0800

    update

---
+++ unicorn-controller/src/main/java/stray/immortal/unicorn/controller/App.java
@@ -0,0 +1,13 @@
+package stray.immortal.unicorn.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ unicorn-core/src/main/java/stray/immortal/unicorn/core/App.java
@@ -0,0 +1,13 @@
+package stray.immortal.unicorn.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
